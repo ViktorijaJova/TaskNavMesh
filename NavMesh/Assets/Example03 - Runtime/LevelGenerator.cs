56f27077148c059ede7c9fb966dcc69d8bf29012
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2021-02-11 14:04:57 +0100

    //

---
+++ NavMesh/Assets/Example03 - Runtime/LevelGenerator.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class LevelGenerator : MonoBehaviour {
+
+
+	public NavMeshSurface surface;
+
+	public int width = 10;
+	public int height = 10;
+
+	public GameObject wall;
+	public GameObject player;
+
+	private bool playerSpawned = false;
+
+	void Start () {
+		GenerateLevel();
+		surface.BuildNavMesh();
+
+
+	}
+
+	void GenerateLevel()
+	{
+		for (int x = 0; x <= width; x+=2)
+		{
+			for (int y = 0; y <= height; y+=2)
+			{
+				if (Random.value > .7f)
+				{
+					Vector3 pos = new Vector3(x - width / 2f, 1f, y - height / 2f);
+					Instantiate(wall, pos, Quaternion.identity, transform);
+				} else if (!playerSpawned) 
+				{
+					Vector3 pos = new Vector3(x - width / 2f, 1.25f, y - height / 2f);
+					Instantiate(player, pos, Quaternion.identity);
+					playerSpawned = true;
+				}
+			}
+		}
+	}
+
+}
